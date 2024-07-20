b18d060901d56b3c02572fc1c1390e80f78bc365
Amaan-E <amaanehsan@protonmail.com>
2024-07-20 10:25:18 +0530

    Add files via upload

--- lab/22.c
+++ lab/22.c
@@ -1,27 +1,26 @@
 // Display Fibonacci series: 0 1 1 2 3 5 8 13 21.... n
-/*
-
-*/
 
 #include <stdio.h>
-int main() 
-{
-	int i, n, j;
-	
-	printf("Enter 'n': ");
-	scanf("%d", &n);
-	
-	
-	for(i = 0; i < n;)// ++i)
+int main() {
+
+	long i, n, a = 0, b = 1;
+
+	long c = a + b;
+
+	printf("Enter the number of terms: ");
+	scanf("%ld", &n);
+
+	// print the first two terms
+	printf("Fibonacci Series: %ld, %ld, ", a, b);
+
+	for (i = 3; i <= n; ++i) 
 	{
-		i = i + j;
-		a & b
-		a + b = c
-		c + c;
-		printf("%d ", i);
+		printf("%ld, ", c);
+		a = b;
+		b = c;
+		c = a + b;
 	}
-	
+
 	return 0;
-	
 }
 
