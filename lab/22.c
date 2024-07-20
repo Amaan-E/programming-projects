2e4e051e94d9a53c49724ff04893578243628968
Amaan-E <amaanehsan@protonmail.com>
2024-07-20 10:33:39 +0530

    Update 22.c

--- lab/22.c
+++ lab/22.c
@@ -1,8 +1,8 @@
 // Display Fibonacci series: 0 1 1 2 3 5 8 13 21.... n
 
 #include <stdio.h>
-int main() {
-
+int main() 
+{
 	int i, n, a = 0, b = 1;
 
 	int c = a + b;
@@ -13,7 +13,7 @@ int main() {
 	// print the first two terms
 	printf("Fibonacci Series: %d, %d, ", a, b);
 
-	for (i = 3; i <= n; ++i) 
+	for (i = 1; i <= n - 2; ++i) //subtract first two terms 
 	{
 		printf("%d, ", c);
 		a = b;
