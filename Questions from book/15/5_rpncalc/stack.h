cd3fe5c62ce7ff3d735b38962ab5e6a42e576085
Amaan-E <174715078+Amaan-E@users.noreply.github.com>
2025-02-19 19:42:56 +0530

    Add 15.

---
+++ Questions from book/15/5_rpncalc/stack.h
@@ -0,0 +1,20 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+
+#define STACK_SIZE 100 
+
+/* external variables */
+extern int contents[];
+extern int top;
+
+void make_empty( void );
+bool is_empty( void );
+bool is_full( void );
+void push( int i );
+int pop( void );
+void stack_overflow( void );
+void stack_underflow( void );
+
+#endif
